5dcd800ddd5ee9c5201c389b053e45b5b581e6fd
Julian Dolby <dolby@us.ibm.com>
2021-03-15 21:16:08 -0400

    progress

--- src/main/java/com/ibm/wala/codeNet/WalaToGNNFiles.java
+++ src/main/java/com/ibm/wala/codeNet/WalaToGNNFiles.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UTFDataFormatException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import com.ibm.wala.cast.java.ecj.util.SourceDirCallGraph;
 import com.ibm.wala.cast.java.ipa.callgraph.JavaSourceAnalysisScope;
@@ -17,8 +21,12 @@ import com.ibm.wala.ipa.cfg.BasicBlockInContext;
 import com.ibm.wala.ipa.cfg.InterproceduralCFG;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import com.ibm.wala.ssa.ISSABasicBlock;
+import com.ibm.wala.ssa.SSAAbstractInvokeInstruction;
+import com.ibm.wala.ssa.SSANewInstruction;
 import com.ibm.wala.util.collections.HashMapFactory;
+import com.ibm.wala.util.collections.HashSetFactory;
 import com.ibm.wala.util.collections.NonNullSingletonIterator;
+import com.ibm.wala.util.graph.traverse.BoundedBFSIterator;
 import com.ibm.wala.util.graph.traverse.DFS;
 
 public class WalaToGNNFiles {
@@ -70,23 +78,109 @@ public class WalaToGNNFiles {
 			Map<BasicBlockInContext<ISSABasicBlock>,Integer> dfsStart = HashMapFactory.make();
 			Iterator<BasicBlockInContext<ISSABasicBlock>> reverseSearch = DFS.iterateDiscoverTime(ipcfg, new NonNullSingletonIterator<>(entry));
 			while (reverseSearch.hasNext()) {
-				dfsFinish.put(reverseSearch.next(), reverseDfsNumber++);
+				dfsStart.put(reverseSearch.next(), reverseDfsNumber++);
 			} 
 
+			Map<BasicBlockInContext<ISSABasicBlock>,Integer> bfsDepth = HashMapFactory.make();
+			BoundedBFSIterator<BasicBlockInContext<ISSABasicBlock>> depths = new BoundedBFSIterator<>(ipcfg, new NonNullSingletonIterator<>(entry), 1000);
+			while(depths.hasNext()) {
+				BasicBlockInContext<ISSABasicBlock> n = depths.next();
+				bfsDepth.put(n, depths.getCurrentHops());
+			}
+			
 			// nodes files
 			withOutput("num-node-list.csv.gz", f -> {
 				f.println("" + dfsFinish.size());
 			});
 			
-			// DFS order file
 			withOutput("node_dfs_order.csv.gz", f -> {
-				ipcfg.stream().filter(n -> dfsFinish.containsKey(n)).forEach(new Consumer<BasicBlockInContext<ISSABasicBlock>>() {
-					public void accept(BasicBlockInContext<ISSABasicBlock> n) {
-						f.println("" + dfsFinish.get(n));
+				ipcfg.stream().filter(n -> dfsFinish.containsKey(n)).forEach(n -> f.println("" + dfsFinish.get(n)));
+				f.flush();
+			});
+
+			withOutput("node_depth.csv.gz", f -> {
+				ipcfg.stream().filter(n -> dfsFinish.containsKey(n)).forEach(n -> f.println("" + bfsDepth.get(n)));
+				f.flush();
+			});
+
+			Function<BasicBlockInContext<ISSABasicBlock>, Set<String>> features = (n) -> {
+				Set<String> fss = HashSetFactory.make();
+				n.iterator().forEachRemaining(inst -> { 
+					if (inst instanceof SSAAbstractInvokeInstruction) {
+						try {
+							fss.add(((SSAAbstractInvokeInstruction) inst).getDeclaredTarget().getName().toUnicodeString());
+						} catch (UTFDataFormatException e) {
+							assert false : e;
+						}
+					} else if (inst instanceof SSANewInstruction) {
+						fss.add(((SSANewInstruction)inst).getConcreteType().getName().toUnicodeString());
 					}
+				});
+				return fss;
+			};
+
+			withOutput("node-feat.csv.gz", f -> {
+				ipcfg.stream().filter(n -> dfsFinish.containsKey(n)).forEach(n -> {
+					features.apply(n).forEach(s -> f.print(s + " "));
+					f.println();
 				});
 				f.flush();
 			});
+
+			withOutput("node_is_attributed.csv.gz", f -> {
+				ipcfg.stream().filter(n -> dfsFinish.containsKey(n)).forEach(n -> {
+					f.println(! features.apply(n).isEmpty());
+				});
+				f.flush();
+			});
+			
+			// edge files
+			class EdgeProcessor {
+				void doit(BiFunction<BasicBlockInContext<ISSABasicBlock>,BasicBlockInContext<ISSABasicBlock>,Void> edges) {
+					ipcfg.stream()
+					.filter(p -> dfsFinish.containsKey(p))
+					.forEach(p -> { 
+						ipcfg.getSuccNodes(p).forEachRemaining(s -> {
+							if (dfsFinish.containsKey(s) &&
+									!( (dfsStart.get(p) < dfsStart.get(s)) &&	
+									   (dfsFinish.get(p) > dfsFinish.get(s)) )) {
+								edges.apply(p, s);
+							}
+						});
+					});
+				}
+			};
+			
+			withOutput("edges.csv.gz", f -> {
+				new EdgeProcessor().doit((p, s) -> {
+					f.println(dfsFinish.get(p) + "," + dfsFinish.get(s));
+					return null;
+				});
+			});
+
+			withOutput("num-edge-list.csv.gz", f -> {
+				Map<BasicBlockInContext<ISSABasicBlock>,Set<BasicBlockInContext<ISSABasicBlock>>> es = HashMapFactory.make();
+				new EdgeProcessor().doit((p, s) -> {
+						if (! es.containsKey(p)) {
+							es.put(p, HashSetFactory.make());
+						}
+						es.get(p).add(s);
+						return null;
+				});
+				ipcfg.stream()
+						.filter(p -> dfsFinish.containsKey(p))
+						.forEach(p -> { 
+							f.println("" + (es.containsKey(p)? es.get(p).size(): 0));
+						});
+			});
+			
+			
+			// graph files
+			withOutput("graph-label.csv.gz", f -> {
+				f.println("" + System.getProperty("graphLabel"));
+			});
+
+			
 		});
 	}
 }
