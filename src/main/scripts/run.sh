81b7022ff5b723a5397bb1b930d7b355f939a133
Julian Dolby <dolby@us.ibm.com>
2021-03-25 22:17:14 -0400

    hacking

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -7,16 +7,16 @@ ROOT=`realpath $DIR/../../..`
 list=$1
 slice=$2
 limit=$3
+ourdir=$4
 
 JAR=${ROOT}/target/AnalysisGraphGenerator-0.0.1-SNAPSHOT.jar
 CLS=com.ibm.wala.codeNet.WalaToGNNFiles
 
 (for f in `awk "NR%${limit}==${slice} { print \\$1; }" ${list}`
  do dir=$ROOT/data/`basename $list`/`basename $f .java`
-    mkdir -p $dir
     jdir=`dirname $f`
     pdir=`dirname $jdir`
     p=`basename $pdir`
-    java -cp $JAR -DgraphLabel=$p -DoutputDir=$dir $CLS -sourceDir=$f -mainClass=LMain
+    java -cp $JAR -DgraphLabel=$p -DoutputDir=$outdir $CLS -sourceDir=$f -mainClass=LMain
  done
 ) > /tmp/log.codenet.`basename $list`.${slice}.txt 2>&1
