d57252aefdefb238a5dffc5dedfb9156cd30eac3
Julian Dolby <dolby@us.ibm.com>
2021-03-31 12:51:10 -0400

    script fixes

--- src/main/scripts/wala1k.sh
+++ src/main/scripts/wala1k.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+DIR=`dirname $0`
+
 lst=$1
 dir=$2
 
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49; do
-    (nohup bash -v src/main/scripts/run.sh $lst $i 50 $dir &);
+shift; shift
+
+hosts="$@"
+nodes=${#hosts}
+slices=`expr 50 '*' $nodes`
+
+slice=0
+for host in $host; do
+    ssh $host $DIR/wala_node.sh $lst $dir $slice $slices &
+    slice=`expr $slice + 50`
 done
+
+wait
 
 for f in `ls $dir/11/* | fgrep -v .csv`; do
     d=`basename $f`
